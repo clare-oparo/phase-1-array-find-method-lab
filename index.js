e656710ca519e6679d5a17870d9f16add967b68e
Clare <clare.oparo@student.moringaschool.com>
2023-12-18 18:45:30 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W" );
+    if(result){
+        return result.year;
+    }
+    return undefined;
+}
+
+
+    
+
